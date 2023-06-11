65db72c2ce41a98e749814073e654fe4a1fcc215
stephanalas <72700716+stephanalas@users.noreply.github.com>
2023-06-11 09:31:58 -0400

    fixed untracked files

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
+import { PrismaClient } from './generated/client/deno/edge.ts';
+
+const prisma = new PrismaClient();
+
+async function handler(request: Request) {
+	const log = await prisma.log.create({
+		data: {
+			level: 'Info',
+			message: `${request.method} ${request.url}`,
+			meta: {
+				headers: JSON.stringify(request.headers),
+			},
+		},
+	});
+	const body = JSON.stringify(log, null, 2);
+	return new Response(body, {
+		headers: { 'content-type': 'application/json; charset=utf-8' },
+	});
+}
+
+serve(handler);
